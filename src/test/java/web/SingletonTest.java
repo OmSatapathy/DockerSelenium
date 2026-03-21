fb0504ba4b7e7b4a7e3963de0113926a733d0192
Om Satapathy <omsatapathy7@gmail.com>
2026-03-21 23:47:18 +0530

    singleton pattern

--- src/test/java/web/SingletonTest.java
+++ src/test/java/web/SingletonTest.java
@@ -4,13 +4,30 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class SingletonTest {
-	
-	
+
 	@Test
 	public void setUp() {
+
+		Runnable task = () -> {
+			SingletonDesign.getInstance();
+			SingletonDesign.runner();
+		};
+
+		Thread t1 = new Thread(task);
+		Thread t2 = new Thread(task);
+		Thread t3 = new Thread(task);
 		
-		SingletonDesign.getInstance();
-		SingletonDesign.runner();
+		t1.start();
+		t2.start();
+		t3.start();
+
+		try {
+			t1.join();
+			t2.join();
+			t3.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
