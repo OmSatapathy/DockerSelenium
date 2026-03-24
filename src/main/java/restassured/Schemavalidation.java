ad420a89c4d0084e75dec334d0e4900ded712632
Om Satapathy <omsatapathy7@gmail.com>
2026-03-24 09:33:42 +0530

    schema validation done

---
+++ src/main/java/restassured/Schemavalidation.java
@@ -0,0 +1,26 @@
+package restassured;
+
+import org.testng.annotations.Test;
+
+import com.github.fge.jsonschema.main.JsonValidator;
+
+import io.restassured.RestAssured;
+import io.restassured.module.jsv.JsonSchemaValidator;
+
+public class Schemavalidation {
+	
+	
+	@Test
+	public void verifyScehema() {
+		
+		RestAssured.given()
+		.contentType("application/json")
+		.when()
+		.get("https://jsonplaceholder.typicode.com/users")
+		.then()
+		.statusCode(200)
+		.body(JsonSchemaValidator.matchesJsonSchemaInClasspath("schema/getuserschema.json"));
+		
+	}
+
+}
